04c5aabf8bca333d4b06ee6934f3ea0beb70ff50
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-31 21:46:33 +0200

    Add files via upload

---
+++ Perviewhtml.vb
@@ -0,0 +1,6 @@
+﻿Public Class Perviewhtml
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        Process.Start("C:\mokawalat\browser\" & ListBox1.SelectedItem)
+    End Sub
+End Class
