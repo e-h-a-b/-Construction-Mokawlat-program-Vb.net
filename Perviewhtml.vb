243dbc989df382d3538150317ef63c47fcf6cd7d
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-31 21:59:41 +0200

    Delete Perviewhtml.vb

--- Perviewhtml.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Perviewhtml
-
-    Private Sub ListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
-        Process.Start("C:\mokawalat\browser\" & ListBox1.SelectedItem)
-    End Sub
-End Class
